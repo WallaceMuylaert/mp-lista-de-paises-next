eb99316f46e973cd03d17c6bc74785301603fbf4
Wallace <wallacedasilva.barbosa@hotmail.com>
2023-10-15 13:49:40 -0300

    Finalizei

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const ninitoSans = Nunito_Sans({ subsets: ['latin'] })
 
@@ -21,8 +22,10 @@ export default function RootLayout({
       <main className="bg-gray-100 min-h-screen flex flex-col items-center">
         <nav className='w-full bg-white h-16 flex items-center justify-center'>
           <section className='container flex items-center gap-3'>
-          <Image width={48} height={48} src="/logo.svg" alt="Logo da aplicação - emoji de globo"/>
+            <Image width={48} height={48} src="/logo.svg" alt="Logo da aplicação - emoji de globo"/>
+            <Link href="/">
             <h1 className='font-bold text-2xl'>Lista de países</h1>
+           </Link>
           </section>
         </nav>
         {children}
